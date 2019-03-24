5b59393e61f185e6565cab885bcc5fafcfb6dbb1
joostterbeek <joostterbeek@hotmail.com>
2019-03-24 16:33:04 +0100

    vijfde foto werkende kermis

--- src/nl/youngcapital/kermis/attracties/GokAttractie.java
+++ src/nl/youngcapital/kermis/attracties/GokAttractie.java
@@ -1,6 +1,6 @@
 package nl.youngcapital.kermis.attracties;
 
 public interface GokAttractie {
-	static double procent = 0.70;
+	static double procent = 0.30;
 	public double kansSpelBelastingBetalen();
 }
